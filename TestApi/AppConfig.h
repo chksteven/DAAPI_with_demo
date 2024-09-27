7b991f608e268420bf1da6263db78e6cc0b050c0
chksteven <38183489+chksteven@users.noreply.github.com>
2024-09-27 15:27:17 +0800

    Add files via upload

---
+++ TestApi/AppConfig.h
@@ -0,0 +1,33 @@
+
+#pragma once
+
+struct CAppConfig
+{
+	string	userId;
+	string	password;
+	string	heartBeat;
+	string	authorCode;
+	string	macAddress;
+	string	computerName;
+	string	softwareName;
+	string	serverAddress;
+	string	softwareVersion;
+	string  priceBrokerId;
+
+	CAppConfig()
+	{
+		
+		heartBeat = "180";
+		softwareName = "api";
+		userId = "";
+		password = "";
+		softwareVersion = "2";
+		macAddress = "song_123";
+		computerName = "WJQ-PC";
+		serverAddress = "192.168.1.1:10070";
+		authorCode = "porl99bbo/jrfib5xxgagza5giggzr/u";
+		priceBrokerId = "xxx";
+
+	}
+};
+
